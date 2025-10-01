c0cb79374a347670d21eb96ce129190630d1bf16
Ognjen Popovic <bapop413@gmail.com>
2025-10-02 01:16:20 +0200

    feat: Implement GameHistoryDbContext

---
+++ Ludus/Services/GameHistoryService/Infrastructure/Data/GameHistoryDbContext.cs
@@ -0,0 +1,35 @@
+using Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class GameHistoryDbContext : DbContext
+    {
+        public GameHistoryDbContext(DbContextOptions<GameHistoryDbContext> options) : base(options)
+        { }
+
+        public DbSet<GameHistory> GameHistories { get; set; }
+        public DbSet<ChatMessage> ChatMessages { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<GameHistory>(eb =>
+            {
+                eb.HasKey(x => x.MatchId);
+                eb.Property(x => x.MatchId).IsRequired();
+                eb.Property(x => x.MoveHistory).HasColumnType("text");
+                eb.Property(x => x.PlayerUserIds).HasConversion(v => string.Join(',', v), v => v.Split(',', StringSplitOptions.RemoveEmptyEntries).ToList()).HasColumnName("PlayerUserIds");
+                eb.HasMany(x => x.ChatMessages).WithOne().HasForeignKey("GameMatchId").OnDelete(DeleteBehavior.Cascade);
+            });
+
+            modelBuilder.Entity<ChatMessage>(eb =>
+            {
+                eb.HasKey("Id");
+                eb.Property<Guid>("Id").HasColumnType("uuid");
+                eb.Property(x => x.SenderUserId).IsRequired();
+                eb.Property(x => x.Message).HasColumnType("text");
+            });
+        }
+    }
+}
+
