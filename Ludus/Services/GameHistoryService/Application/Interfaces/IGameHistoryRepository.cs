b1910e47d147adcb8074e07a61d8733cff9e48ae
Ognjen Popovic <bapop413@gmail.com>
2025-10-02 01:13:24 +0200

    feat: Add interface for GameHistoryRepository

---
+++ Ludus/Services/GameHistoryService/Application/Interfaces/IGameHistoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using Entities;
+
+namespace Interfaces
+{
+    public interface IGameHistoryRepository
+    {
+        Task SaveGameAsync(GameHistory gameHistory);
+        Task AppendChatMessageAsync(string matchId, IEnumerable<ChatMessage> messages);
+        Task<IEnumerable<GameHistory>> GetGamesByUserAsync(string userId, int limit = 50, int offset = 0);
+        Task<GameHistory?> GetByMatchIdAsync(string matchId);
+    }
+}
