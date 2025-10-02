fbeaed3e2407fb4d6ee72f889e1562dbdc23b385
Ognjen Popovic <bapop413@gmail.com>
2025-10-02 02:02:03 +0200

    feat: Add auth in Program.cs

--- Ludus/Services/GameHistoryService/Program.cs
+++ Ludus/Services/GameHistoryService/Program.cs
@@ -1,4 +1,7 @@
 using Microsoft.EntityFrameworkCore;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 using Messaging;
 using Data;
@@ -15,6 +18,38 @@ internal class Program
         // RabbitMQ configuration
         builder.Services.Configure<RabbitMQSettings>(builder.Configuration.GetSection("RabbitMQ"));
 
+        // JWT settings
+        var jwtSection = builder.Configuration.GetSection("Jwt");
+        var jwtKey = jwtSection.GetValue<string>("Key");
+        if (string.IsNullOrEmpty(jwtKey))
+        {
+            Console.WriteLine("Warning: JWT Key not configured. API will run without authentication if no key is provided.");
+        }
+        else
+        {
+            var keyBytes = Encoding.UTF8.GetBytes(jwtKey);
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = !string.IsNullOrEmpty(jwtSection.GetValue<string>("Issuer")),
+                    ValidIssuer = jwtSection.GetValue<string>("Issuer"),
+                    ValidateAudience = !string.IsNullOrEmpty(jwtSection.GetValue<string>("Audience")),
+                    ValidAudience = jwtSection.GetValue<string>("Audience"),
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(keyBytes),
+                    ValidateLifetime = true
+                };
+            });
+        }
+
         // DbContext
         builder.Services.AddDbContext<GameHistoryDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DbConnectionString")));
 
@@ -22,9 +57,9 @@ internal class Program
         builder.Services.AddScoped<IGameHistoryRepository, GameHistoryRepository>();
         builder.Services.AddScoped<IGameHistoryService, GameHistoryService>();
 
+        // Hosted consumers for RabbitMQ
         builder.Services.AddHostedService<GameEndedEventConsumer>();
         builder.Services.AddHostedService<ChatLogEventConsumer>();
-
 
         builder.Services.AddControllers();
         builder.Services.AddEndpointsApiExplorer();
@@ -32,7 +67,6 @@ internal class Program
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
@@ -40,6 +74,13 @@ internal class Program
         }
 
         app.UseRouting();
+
+        if (!string.IsNullOrEmpty(jwtKey))
+        {
+            app.UseAuthentication();
+            app.UseAuthorization();
+        }
+
         app.MapControllers();
 
         using (var scope = app.Services.CreateScope())
