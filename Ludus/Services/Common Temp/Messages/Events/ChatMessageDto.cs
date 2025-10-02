ead71456cab77711dc6010ef3dfb4a20450692bb
Ognjen Popovic <bapop413@gmail.com>
2025-10-02 02:34:26 +0200

    cleanup: Remove unneeded files

--- Ludus/Services/Common Temp/Messages/Events/ChatMessageDto.cs
+++
@@ -1,6 +0,0 @@
-public class ChatMessageDto
-{
-    public string SenderUserId { get; set; }
-    public string Message { get; set; }
-    public DateTime SentAt { get; set; }
-}
--- Ludus/Services/Common Temp/Messages/Events/GameEndedEvent.cs
+++
@@ -1,9 +0,0 @@
-public class GameEndedEvent
-{
-    public string MatchId { get; set; }
-    public List<string> PlayerUserIds { get; set; }
-    public DateTime StartedAt { get; set; }
-    public DateTime EndedAt { get; set; }
-    public Dictionary<string, int> Scores { get; set; }
-    public string MoveHistory { get; set; }
-}
