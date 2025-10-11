8e0fe84e7724128db8683e99baa69364336f22eb
Ognjen Popovic <bapop413@gmail.com>
2025-10-11 03:25:36 +0200

    fix: Add missing default values for GameHistory class

--- Ludus/Services/Common/Entities/GameHistory.cs
+++ Ludus/Services/Common/Entities/GameHistory.cs
@@ -5,13 +5,13 @@ namespace Common.Entities
     public class GameHistory
     {
         [Key]
-        public string MatchId { get; set; }
-        public List<string> PlayerUserIds { get; set; }
+        public string MatchId { get; set; } = string.Empty;
+        public List<string> PlayerUserIds { get; set; } = new();
         public DateTime StartedAt { get; set; }
         public DateTime EndedAt { get; set; }
-        public string MoveHistory { get; set; }
+        public string MoveHistory { get; set; } = string.Empty;
         public string? WinnerUserId { get; set; }
-        public List<ChatMessage> ChatMessages { get; set; }
+        public List<ChatMessage> ChatMessages { get; set; } = new();
     }
 
 }
