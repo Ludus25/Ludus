8f8dfb535649b6349a607077bfe7b03fda3a2750
markobura <markobura99@gmail.com>
2025-10-14 14:20:45 +0200

    Change board and game components

--- Ludus/Frontend/ludus-frontend/src/features/game/ui/Board.tsx
+++ Ludus/Frontend/ludus-frontend/src/features/game/ui/Board.tsx
@@ -5,20 +5,22 @@ export default function Board({
   cells, disabled, onClickCell
 }: { cells: CellEnum[]; disabled?: boolean; onClickCell: (i:number)=>void }) {
   return (
-    <div style={{ display:'grid', gridTemplateColumns:'repeat(3, 96px)', gap:12 }}>
-      {cells.map((c, i) => (
-        <Button
-          key={i}
-          type={c === 0 ? 'default' : 'primary'}
-          size="large"
-          shape="round"
-          onClick={() => onClickCell(i)}
-          disabled={disabled || c !== 0}
-          style={{ height: 96, fontSize: 28, fontWeight: 700 }}
-        >
-          {renderCell(c)}
-        </Button>
-      ))}
+    <div style={{display:'flex',justifyContent:'center'}}>
+      <div style={{ display:'grid', gridTemplateColumns:'repeat(3, 96px)', gap:12 }}>
+        {cells.map((c, i) => (
+          <Button
+            key={i}
+            type={c === 0 ? 'default' : 'primary'}
+            size="large"
+            shape="round"
+            onClick={() => onClickCell(i)}
+            disabled={disabled || c !== 0}
+            style={{ height: 96, fontSize: 28, fontWeight: 700 }}
+          >
+            {renderCell(c)}
+          </Button>
+        ))}
+      </div>
     </div>
   )
 }
